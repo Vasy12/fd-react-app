d639f82883adb977d76885fa2fca74df7d11fef7
User Name <artur.it2914@gmail.com>
2020-07-31 17:15:51 +0300

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,8 @@
 import React, { Component } from 'react';
-import UsersList from './components/UsersList';
-import SelectedUsersList from './components/SelectedUsersList';
-class App extends Component {
-  constructor(props) {
-    super(props);
+import Stopwatch from './components/Stopwatch';
 
-    this.state = {
-      users: [
-        {
-          id: 1,
-          firstName: 'Test',
-          lastName: 'Testovich',
-          isSelected: false,
-        },
-        {
-          id: 2,
-          firstName: 'Test2',
-          lastName: 'Testovich2',
-          isSelected: false,
-        },
-        {
-          id: 3,
-          firstName: 'Test3',
-          lastName: 'Testovich3',
-          isSelected: false,
-        },
-        {
-          id: 4,
-          firstName: 'Test4',
-          lastName: 'Testovich4',
-          isSelected: false,
-        },
-      ],
-    };
-  }
-
-  setUsers = users => {
-    this.setState({
-      users,
-    });
-  };
-
-  render() {
-    const { users } = this.state;
-    return (
-      <>
-        <UsersList users={users} setUsers={this.setUsers} />
-        <SelectedUsersList users={users} />
-      </>
-    );
-  }
+function App() {
+  return <Stopwatch />;
 }
 
 export default App;
